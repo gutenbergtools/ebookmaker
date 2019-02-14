5e76e9955d7819ab74dbe306017d1289a8f95c28
eric <eric@hellman.net>
2019-02-14 16:16:20 -0500

    added test

--- ebookmaker/tests/test_htm.py
+++ ebookmaker/tests/test_htm.py
@@ -35,6 +35,35 @@ class TestFromHtm(unittest.TestCase):
         for out in outs:
             self.assertTrue(os.path.exists(os.path.join(dir, out % book_id)))
             os.remove(os.path.join(dir, out % book_id))
+
+    def test_43172_nocover(self):
+        book_id = '43172'
+        dir = os.path.join(self.sample_dir, book_id)
+        htmfile = os.path.join(dir, '%s-h' % book_id, '%s-nocover.htm' % book_id)
+        cmd = 'ebookmaker --make=test --output-dir={dir} --generate_cover {htmfile}'.format(
+            dir=dir,
+            htmfile=htmfile,
+        )
+
+        output = subprocess.check_output(cmd, shell=True)
+
+        self.assertFalse(output)
+        outs = [
+            "%s-epub.epub",
+            "%s-images-epub.epub",
+            "%s-noimages-h.html",
+            "%s-h.html",
+            "%s.txt",
+            "%s-8.txt",
+            "%s-0.txt",
+            "%s-cover.png",
+        ]
+        for out in outs:
+            self.assertTrue(os.path.exists(os.path.join(dir, out % book_id)))
+            os.remove(os.path.join(dir, out % book_id))
+        gencover = os.path.join(dir,  '%s-h' % book_id, "%s-cover.png" % book_id)
+        self.assertTrue(os.path.exists(gencover))
+        os.remove(gencover)
                 
             
         
