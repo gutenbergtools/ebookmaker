c4b61efe4a6b7d7fd1819f49dd46cdd26fa98e9a
eric <eric@hellman.net>
2020-09-14 15:52:47 -0400

    added rst test

---
+++ ebookmaker/tests/test_rst.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import unittest
+import subprocess
+
+
+import ebookmaker
+
+class TestFromRst(unittest.TestCase):
+    def setUp(self):
+        self.sample_dir = os.path.join(os.path.dirname(__file__),'samples')
+
+    def test_33968(self):
+        book_id = '33968'
+        dir = os.path.join(self.sample_dir, book_id)
+        rstfile = os.path.join(dir, '%s-rst' % book_id, '%s-rst.rst' % book_id)
+        cmd = 'ebookmaker --make=pdf --output-dir={dir} {rstfile}'.format(
+            dir=dir,
+            rstfile=rstfile,
+        )
+
+        output = subprocess.check_output(cmd, shell=True)
+
+        self.assertFalse(output)
+        outs = [
+            "%s-pdf.pdf",
+            "%s-h.html",
+            "%s-cover.png",
+            "%s-images-pdf.pdf",
+            "%s-epub.epub",
+            "%s-images-epub.epub",
+            "%s-noimages-h.html",
+        ]
+        for out in outs:
+            self.assertTrue(os.path.exists(os.path.join(dir, out % book_id)))
+            os.remove(os.path.join(dir, out % book_id))
+        
