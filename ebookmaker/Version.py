70570d174c7ca6ec6ca743037c495e27e1e86bc4
eric <eric@hellman.net>
2021-10-28 16:01:23 -0400

    0.11.22

--- ebookmaker/Version.py
+++ ebookmaker/Version.py
@@ -1,2 +1,2 @@
-VERSION = '0.11.21'
+VERSION = '0.11.22'
 GENERATOR = 'Ebookmaker %s by Project Gutenberg'
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 
 from setuptools import setup
 
-VERSION = '0.11.21'
+VERSION = '0.11.22'
 
 setup (
     name = 'ebookmaker',
