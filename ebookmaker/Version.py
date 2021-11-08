365788863deec66e4a7051ec850e0991c967af30
eric <eric@hellman.net>
2021-11-08 15:23:09 -0500

    0.11.24

--- ebookmaker/Version.py
+++ ebookmaker/Version.py
@@ -1,2 +1,2 @@
-VERSION = '0.11.23'
+VERSION = '0.11.24'
 GENERATOR = 'Ebookmaker %s by Project Gutenberg'
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 
 from setuptools import setup
 
-VERSION = '0.11.23'
+VERSION = '0.11.24'
 
 setup (
     name = 'ebookmaker',
@@ -43,7 +43,7 @@ setup (
         'roman',
         'requests',
         'six>=1.4.1',
-        'libgutenberg[covers]>=0.8.11',
+        'libgutenberg[covers]>=0.8.12',
     ],
     
     package_data = {
