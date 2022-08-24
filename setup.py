c8f54a68f9d06b02c8927f5708b9ceca02335f90
eric <eric@hellman.net>
2022-08-24 09:02:52 +0200

    0.12.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 
 from setuptools import setup
 
-VERSION = '0.12.2'
+VERSION = '0.12.3'
 
 if __name__ == "__main__":
  
--- src/ebookmaker/Version.py
+++ src/ebookmaker/Version.py
@@ -1,2 +1,2 @@
-VERSION = '0.12.2'
+VERSION = '0.12.3'
 GENERATOR = 'Ebookmaker %s by Project Gutenberg'
