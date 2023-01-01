70dfcdab0ea51e9b930a3f4778a912c4acba0c8d
eric <eric@hellman.net>
2023-01-01 16:41:39 -0500

    strip marc subfields from pubinfo

--- src/ebookmaker/writers/HtmlTemplates.py
+++ src/ebookmaker/writers/HtmlTemplates.py
@@ -58,7 +58,7 @@ This ebook is for the use of anyone anywhere in the United States and most other
         {pstyle('Release Date', 
             f'{dc.release_date.strftime(hr_format)} [EBook #{dc.project_gutenberg_id}]')}
         {pstyle('Language', ', '.join(language_list))}
-        {pstyle('Original Publication', dc.pubinfo.marc())}
+        {pstyle('Original Publication', dc.strip_marc_subfields(dc.pubinfo.marc()))}
         {pstyle('Credits', dc.credit)}
     </div>
     <div class="vspace" style="height: 2em"><br /></div>
