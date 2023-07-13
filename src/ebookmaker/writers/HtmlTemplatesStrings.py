b85a381e1176729bfdc4b106a5ba154d2a55feb3
Greg Newby <gbnewby@petascale.org>
2023-07-13 13:37:40 -0700

    Create HtmlTemplatesStrings.py
    Added new stub file; will populate with strings from HtmlTemplates.py that are referenced there.

---
+++ src/ebookmaker/writers/HtmlTemplatesStrings.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#  -*- mode: python; indent-tabs-mode: nil; -*- coding: utf-8 -*-
+
+"""
+HtmlTemplatesStrings.py
+
+Copyright 2023 by Project Gutenberg
+
+Relocate static content from HtmlTemplates.py to this file, for easier
+maintenance and use by applications that need the license, header/footer,
+CSS, etc. but not the functionality of HtmlTemplates.py
+"""
+
