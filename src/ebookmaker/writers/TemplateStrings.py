343db12dd0f8c9490fd1ec8917ed523f6dfabc83
Greg Newby <gbnewby@petascale.org>
2025-07-03 11:07:36 -0700

    Update TemplateStrings.py
    An alert reader noted that we managed to use "ebook" and "eBook" twice each in the opening header paragraph.
    
    Let's make them all camel case, like in the small print license.
    
    I do want to update in the foreseeable future to just say, "book." That's a bigger site update, though, and not needed to address the more immediate issue of inconsistency in the opening paragraph of the header.

--- src/ebookmaker/writers/TemplateStrings.py
+++ src/ebookmaker/writers/TemplateStrings.py
@@ -83,10 +83,10 @@ CSS_FOR_HEADER = '''
 '''
 
 HEADERA = '''
-<div>This ebook is for the use of anyone anywhere in the United States and
+<div>This eBook is for the use of anyone anywhere in the United States and
 most other parts of the world at no cost and with almost no restrictions
 whatsoever. You may copy it, give it away or re-use it under the terms
-of the Project Gutenberg License included with this ebook or online
+of the Project Gutenberg License included with this eBook or online
 at <a class="reference external" href="https://www.gutenberg.org">www.gutenberg.org</a>. If you are not located in the United States,
 you will have to check the laws of the country where you are located
 before using this eBook.</div>
